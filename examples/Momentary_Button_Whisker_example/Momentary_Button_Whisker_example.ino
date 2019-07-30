935b31fd6461db12130caa0685910cefda93fc58
TinyCircuits <info@tinycircuits.com>
2019-07-30 15:09:17 -0400

    updated to use Port 0

---
+++ examples/Momentary_Button_Whisker_example/Momentary_Button_Whisker_example.ino
@@ -0,0 +1,44 @@
+//************************************************************************
+// Momentary Button Whisker Example
+//
+// Uses digitalRead to detect when the momentary button is pressed
+// and reports it to the serial monitor.
+//
+// Written by Jason Marcum 7/30/19
+//************************************************************************
+
+#define buttonPin A0 // corresponds to Port 0. Similarly, Port 1 = A1, Port 2 = A2, Port 3 = A3.
+
+// Used to track the state of the button
+int buttonState = 0;
+
+
+void setup() {
+  Wire.begin();                 //Begin I2C Communication
+  SerialUSB.begin(9600);         //Begin Serial Communication and set Baud Rate
+
+  // Use this pin as an input to read the button state
+  pinMode(buttonPin, INPUT);
+}
+
+
+void loop() {
+
+  // Used for alearting user/printing to serial monitor if the button state changes
+  int prevButtonState = buttonState;
+
+  // Get the button state
+  buttonState = digitalRead(buttonPin);
+
+  if (prevButtonState != buttonState) // No need to alert the user if button state remains unchanged
+  {
+    if (buttonState == LOW)
+    {
+      SerialUSB.println("Button pressed!");
+    }
+    else
+    {
+      // Button not currently being pressed
+    }
+  }
+}
